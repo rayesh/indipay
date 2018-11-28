07d0dbbf6b95c6372cea9a55bd8ac1a36e957db3
Rajesh R <rayesh@gmail.com>
2018-11-28 20:16:21 +0530

    Experimenting with automatic button click

--- src/views/razorpay.blade.php
+++ src/views/razorpay.blade.php
@@ -5,42 +5,65 @@
 </head>
 <body>
     <form action="{{ $parameters['redirect_url'] }}" method="POST">
-        <script
-            src="https://checkout.razorpay.com/v1/checkout.js"
-            data-key="{{ $keyId }}"
-            data-amount="{{ $amount }}"
-            data-name="{{ $parameters['merchant_name'] }}"
-            @if(!empty($parameters['description']))
-                data-description="{{ $parameters['description'] }}"
-            @endif
-            @if(!empty($parameters['image']))
-                data-image="{{ $parameters['image'] }}"
-            @endif
-            @if(!empty($parameters['button_text']))
-                data-buttontext="{{ $parameters['button_text'] }}"
-            @else
-                data-buttontext="Pay with Razorpay"    
-            @endif
-            @if(!empty($parameters['customer_name']))
-                data-prefill.name="{{ $parameters['customer_name'] }}"
-            @endif
-            @if(!empty($parameters['email']))
-                data-prefill.email="{{ $parameters['email'] }}"
-            @endif
-            @if(!empty($parameters['phone']))
-                data-prefill.contact="{{ $parameters['phone'] }}"
-            @endif
-            @if(!empty($parameters['theme_color'] ))    
-                data-theme.color="{{ $parameters['theme_color'] }}"
-            @else
-                data-theme.color="#F37254"
-            @endif
-        >
-        </script>
-    @foreach($parameters as $param_key=>$param_value)
+        @foreach($parameters as $param_key=>$param_value)
         <input type="hidden" name="{{ $param_key }}" value="{{ $param_value  }}" />
-    @endforeach
+        @endforeach
     </form>
+    <script>
+        var form = document.getElementsByTagName("form")[0];
+        var script = document.createElement("script");
+        script.src = "https://checkout.razorpay.com/v1/checkout.js";
+        script.setAttribute("data-key","{{ $keyId }}");
+        script.setAttribute("data-amount","{{ $amount }}");
+        script.setAttribute("data-name","{{ $parameters['merchant_name'] }}");
+        @if(!empty($parameters['description']))
+        script.setAttribute("data-description","{{ $parameters['description'] }}");
+        @endif
+        @if(!empty($parameters['image']))
+        script.setAttribute("data-image","{{ $parameters['image'] }}");
+        @endif
+        @if(!empty($parameters['button_text']))
+        script.setAttribute("data-buttontext","{{ $parameters['button_text'] }}");
+        @else
+        script.setAttribute("data-buttontext","Pay with Razorpay");
+        @endif
+        @if(!empty($parameters['customer_name']))
+        script.setAttribute("data-prefill.name","{{ $parameters['customer_name'] }}");
+        @endif
+        @if(!empty($parameters['email']))
+        script.setAttribute("data-prefill.email","{{ $parameters['email'] }}");
+        @endif
+        @if(!empty($parameters['phone']))
+        script.setAttribute("data-prefill.contact","{{ $parameters['phone'] }}");
+        @endif
+        @if(!empty($parameters['theme_color'] ))    
+        script.setAttribute("data-theme.color","{{ $parameters['theme_color'] }}");
+        @else
+        script.setAttribute("data-theme.color","#F37254");
+        @endif
+        // Handle Script loading
+        var done = false;
+
+        // Attach handlers for all browsers
+        script.onload = script.onreadystatechange = function() {
+            if ( !done && (!this.readyState ||
+                    this.readyState === "loaded" || this.readyState === "complete") ) {
+                done = true;
+                console.log("Script loaded");
+                document.getElementsByClassName("razorpay-payment-button")[0].click();
+                // Handle memory leak in IE
+                script.onload = script.onreadystatechange = null;
+                if ( form && script.parentNode ) {
+                    form.removeChild( script );
+                }
+            }
+        };
+
+        // Use insertBefore instead of appendChild  to circumvent an IE6 bug.
+        // This arises when a base node is used (#2709 and #4378).
+        form.insertBefore( script, form.firstChild );
+
+    </script>
 </body>
 </html>
 
